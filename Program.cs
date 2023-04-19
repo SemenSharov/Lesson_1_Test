fc621c31f2d0baa0f2b89df1656a25ae507ff6a3
SemenSharov <xxxSharik@gmail.com>
2023-04-20 00:18:28 +0300

    Created an array output function

--- Program.cs
+++ Program.cs
@@ -2,10 +2,9 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-
 string[] array1 = new string[5] { "1", "2", "3333", "4444", "5" };
 string[] array2 = new string[array1.Length];
-// void SecondArrayWithIF(string[] array1, string[] array2)
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
@@ -17,4 +16,13 @@ string[] array2 = new string[array1.Length];
         }
     }
 }
-
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
